b35653f00d91d7ead606fee745962e980777ea35
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2022-11-22 15:59:19 +0000

    Update MyNav.jsx

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -7,6 +7,7 @@ const MyNav = ( { props } ) => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
+            <Nav.Link href="#">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#browse">Browse</Nav.Link>
           </Nav>
